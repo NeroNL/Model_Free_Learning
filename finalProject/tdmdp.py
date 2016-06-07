31f0280837d6e99fbd4f6f831fdb52841c5198ad
Jiapeng Li <ljp2745559@gmail.com>
2016-06-07 00:01:39 -0700

    v5, algo still has some problems

--- finalProject/tdmdp.py
+++ finalProject/tdmdp.py
@@ -24,34 +24,34 @@ def tdmdp():
     discount_factor = config["discount_factor"]
     learning_rate = config["learning_rate"]
     mdp_pub = rospy.Publisher("/results/policy_list", PolicyList, queue_size = 100);
-    
+
     row = map_size[0]
     col = map_size[1]
     sX = start[0]
     sY = start[1]
     gX = goal[0]
     gY = goal[1]
-    
+
     mdpMap = []
     for i in range(row):
         mdpMap.append(np.zeros(col, dtype = 'float32'))
     mdpMap = np.array(mdpMap)
 
-for i in range(row):
-    for j in range(col):
-        mdpMap[i][j] = reward_for_each_step
-    
+    for i in range(row):
+        for j in range(col):
+            mdpMap[i][j] = reward_for_each_step
+
     for wall in walls:
         mdpMap[wall[0]][wall[1]] = reward_for_hitting_wall
 
-for pit in pits:
-    mdpMap[pit[0]][pit[1]] = reward_for_falling_in_pit
-    
+    for pit in pits:
+        mdpMap[pit[0]][pit[1]] = reward_for_falling_in_pit
+
     mdpMap[gX][gY] = reward_for_reaching_goal
-    
+
     countMap = np.zeros([row, col, 4], dtype = 'float32')
     oldCountMap = np.copy(countMap)
-    
+
     
     probUp = 0.8#rand.random();
     probDown = 0#rand.random();
@@ -66,11 +66,11 @@ for pit in pits:
     
     policyMap = np.zeros([row, col, 4], dtype = 'float32')
     oldPolicyMap = np.zeros([row, col, 4], dtype = 'float32')
-    
+
     iteration_size = 100
     
-    
-    for w in range(100):
+
+    for w in range(10):
         for i in range(row):
             for j in range(col):
                 if([i,j] in walls):
@@ -87,16 +87,16 @@ for pit in pits:
                         #for n in neighbors[i*col+j]:
                         upValue = moveUp(mdpMap, oldPolicyMap, i, j, row, col, discount_factor, reward_for_hitting_wall, walls, learning_rate)
                         #a = computeReward(probList, upValue)
-                        
+
                         downValue = moveDown(mdpMap, oldPolicyMap,i,j, row, col, discount_factor, reward_for_hitting_wall, walls, learning_rate)
                         #b = computeReward(probList, downValue)
-                        
+
                         leftValue = moveLeft(mdpMap, oldPolicyMap, i, j, row, col, discount_factor, reward_for_hitting_wall, walls, learning_rate)
                         #c = computeReward(probList, leftValue)
-                        
+
                         rightValue = moveRight(mdpMap, oldPolicyMap, i, j, row, col, discount_factor, reward_for_hitting_wall, walls, learning_rate)
                         #d = computeReward(probList, rightValue)
-                        
+
                         
                         for d in range(4):
                             num = rand.random();
@@ -113,16 +113,16 @@ for pit in pits:
                             elif(num >= probUp + probDown + probLeft and num < probUp + probDown + probLeft + probRight):
                                 reward = rightValue[d]
                                 policyMap[i][j][d] += reward
-                
-                policyMap[i][j][0] /= iteration_size
-                policyMap[i][j][1] /= iteration_size
-                policyMap[i][j][2] /= iteration_size
-            policyMap[i][j][3] /= iteration_size
+
+                    policyMap[i][j][0] /= iteration_size
+                    policyMap[i][j][1] /= iteration_size
+                    policyMap[i][j][2] /= iteration_size
+                    policyMap[i][j][3] /= iteration_size
 
 
-    oldPolicyMap = np.copy(policyMap);
+        oldPolicyMap = np.copy(policyMap);
+    
         
-        #print policyMap
         maxScore = np.max(policyMap, axis=2)
         maxIndex = policyMap.argmax(axis=2)
         toBeReturn = []
@@ -131,13 +131,13 @@ for pit in pits:
                 if([k,l] not in walls and [k,l] not in pits and [k,l] != goal):
                     if maxIndex[k,l] == 0:
                         toBeReturn.append('N');
-                    
+                        
                     elif maxIndex[k,l] == 1:
                         toBeReturn.append('S');
-                    
+                        
                     elif maxIndex[k,l] == 2:
                         toBeReturn.append('W');
-                    
+                        
                     elif maxIndex[k,l] == 3:
                         toBeReturn.append('E');
                 elif([k,l] in walls):
@@ -149,12 +149,13 @@ for pit in pits:
         mdp_inst = PolicyList()
         mdp_inst.data = toBeReturn
         mdp_pub.publish(mdp_inst)
+    
+    print policyMap
+    print toBeReturn
 
-#print toBeReturn
+    #print countMap
 
-#print countMap
-
-return 1
+    return 1
 
 
 def computeReward(cl, li):
@@ -164,78 +165,35 @@ def computeReward(cl, li):
 def learning(learningRate, currentIndexValue, reward, df, nextIndexValue):
     return currentIndexValue + learningRate * (reward + df * nextIndexValue - currentIndexValue);
 
-
-
-'''
-    def moveUp(mdpMap, oldPolicyMap, x,y,row,col, df, rhw, walls, learningRate):
-    forward = (x-1,y)
-    
-    if(forward[0] >= 0 and (list(forward) not in walls)):
-    return learning(learningRate, oldPolicyMap[x][y][0], \
-    mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]]))
-    else:
-    return learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y]))
-    
-    
-    
-    def moveDown(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
-    backward = (x+1,y)
-    
-    if(backward[0] < row and (list(backward) not in walls)):
-    return learning(learningRate, oldPolicyMap[x][y][0], \
-    mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]]))
-    else:
-    return learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y]))
-    
-    
-    def moveLeft(mdpMap, oldPolicyMap, x,y,row,col, df, rhw, walls, learningRate):
-    below = (x, y-1)
-    
-    if(below[1] >= 0 and (list(below) not in walls)):
-    return learning(learningRate, oldPolicyMap[x][y][0], \
-    mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]]))
-    else:
-    return learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y]))
-    
-    
-    def moveRight(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
-    below = (x, y+1)
-    
-    if(below[1] < col and (list(below) not in walls)):
-    return learning(learningRate, oldPolicyMap[x][y][0], \
-    mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]]))
-    else:
-    return learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y]))
-    '''
 def moveUp(mdpMap, oldPolicyMap, x,y,row,col, df, rhw, walls, learningRate):
     forward = (x-1,y)
     backward = (x+1,y)
     below = (x, y-1)
     above = (x, y+1)
-    
+
     toBeReturn = []
     if(forward[0] >= 0 and (list(forward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
+            mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
     if(backward[0] < row and (list(backward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
-else:
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-    if(below[1] >= 0 and (list(below) not in walls)):
-        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+            mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-if(above[1] < col and (list(above) not in walls)):
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                               mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
-                               else:
-                                   toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(below[1] >= 0 and (list(below) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(above[1] < col and (list(above) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
 
-return toBeReturn
+    return toBeReturn
 
 
 def moveDown(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
@@ -243,30 +201,30 @@ def moveDown(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
     backward = (x-1,y)
     below = (x, y+1)
     above = (x, y-1)
-    
+
     toBeReturn = []
     if(forward[0] < row and (list(forward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
+            mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
     if(backward[0] >= 0 and (list(backward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
-else:
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-    if(below[1] < col and (list(below) not in walls)):
-        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+            mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-if(above[1] >= 0 and (list(above) not in walls)):
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                               mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
-                               else:
-                                   toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(below[1] < col and (list(below) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(above[1] >= 0 and (list(above) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
 
-return toBeReturn
+    return toBeReturn
 
 
 def moveLeft(mdpMap, oldPolicyMap, x,y,row,col, df, rhw, walls, learningRate):
@@ -274,30 +232,30 @@ def moveLeft(mdpMap, oldPolicyMap, x,y,row,col, df, rhw, walls, learningRate):
     backward = (x,y+1)
     below = (x+1, y)
     above = (x-1, y)
-    
+
     toBeReturn = []
     if(forward[1] >= 0 and (list(forward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
+            mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
     if(backward[1] < col and (list(backward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
-else:
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-    if(below[0] < row and (list(below) not in walls)):
-        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+            mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-if(above[0] >= 0 and (list(above) not in walls)):
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                               mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
-                               else:
-                                   toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(below[0] < row and (list(below) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(above[0] >= 0 and (list(above) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
 
-return toBeReturn
+    return toBeReturn
 
 
 def moveRight(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
@@ -305,27 +263,27 @@ def moveRight(mdpMap, oldPolicyMap, x,y,row,col, df, rhw,walls, learningRate):
     backward = (x,y-1)
     below = (x-1, y)
     above = (x+1, y)
-    
+
     toBeReturn = []
     if(forward[1] < col and (list(forward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
+            mdpMap[forward[0]][forward[1]], df, max(oldPolicyMap[forward[0]][forward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
     if(backward[1] >= 0 and (list(backward) not in walls)):
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
-else:
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-    if(below[0] >= 0 and (list(below) not in walls)):
-        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                                   mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+            mdpMap[backward[0]][backward[1]], df, max(oldPolicyMap[backward[0]][backward[1]])))
     else:
         toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-if(above[0] < row and (list(above) not in walls)):
-    toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
-                               mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
-                               else:
-                                   toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
-                               
-return toBeReturn
+    if(below[0] >= 0 and (list(below) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[below[0]][below[1]], df, max(oldPolicyMap[below[0]][below[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+    if(above[0] < row and (list(above) not in walls)):
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], \
+            mdpMap[above[0]][above[1]], df, max(oldPolicyMap[above[0]][above[1]])))
+    else:
+        toBeReturn.append(learning(learningRate, oldPolicyMap[x][y][0], rhw, df, max(oldPolicyMap[x][y])))
+
+    return toBeReturn
